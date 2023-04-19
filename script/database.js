73b0ab8f2805e788c36938d479b8c2ba5fcf4346
rayanMishra <rayanmishra7@gmail.com>
2023-04-19 16:14:30 -0400

    changed qty to 0 and sorted the inventory variable issue

--- script/database.js
+++ script/database.js
@@ -19,75 +19,76 @@ const addToDatabase = (key, value) => {
 };
 
 // DATA SECTION
-const cheapPrice = () => parseFloat(Math.random() * (5 - 1) + 1).toFixed(2);
+export const cheapPrice = () =>
+  parseFloat(Math.random() * (5 - 1) + 1).toFixed(2);
 const expPrice = () => parseFloat(Math.random() * (15 - 10) + 10).toFixed(2);
 
-export const totalInventory = [
-  {
-    productName: "Tomatoes",
-    id: 0,
-    qty: 1,
-    src: "./organic-project/assets/product1.jpeg",
-    price: cheapPrice(),
-    type: "$",
-  },
-  {
-    productName: "Lime",
-    id: 1,
-    qty: 1,
-    src: "./organic-project/assets/product2.jpeg",
-    price: cheapPrice(),
-    type: "$",
-  },
-  {
-    productName: "Organic Eggplant",
-    id: 2,
-    qty: 1,
-    src: "./organic-project/assets/product3.jpeg",
-    price: expPrice(),
-    type: "$$",
-  },
-  {
-    productName: "Cucumber",
-    id: 3,
-    qty: 1,
-    src: "./organic-project/assets/product4.jpeg",
-    price: cheapPrice(),
-    type: "$",
-  },
-  {
-    productName: "Organic Peas",
-    id: 4,
-    qty: 1,
-    src: "./organic-project/assets/product5.jpeg",
-    price: expPrice(),
-    type: "$$",
-  },
-  {
-    productName: "Lettuce",
-    id: 5,
-    qty: 1,
-    src: "./organic-project/assets/product6.jpeg",
-    price: cheapPrice(),
-    type: "$",
-  },
-  {
-    productName: "Cabbage",
-    id: 6,
-    qty: 1,
-    src: "./organic-project/assets/product7.jpeg",
-    price: cheapPrice(),
-    type: "$",
-  },
-  {
-    productName: "Organic Lettuce",
-    id: 7,
-    qty: 1,
-    src: "./organic-project/assets/product8.jpeg",
-    price: expPrice(),
-    type: "$$",
-  },
-];
+// const totalInventory = [
+//   {
+//     productName: "Tomatoes",
+//     id: 0,
+//     qty: 0,
+//     src: "./organic-project/assets/product1.jpeg",
+//     price: cheapPrice(),
+//     type: "$",
+//   },
+//   {
+//     productName: "Lime",
+//     id: 1,
+//     qty: 0,
+//     src: "./organic-project/assets/product2.jpeg",
+//     price: cheapPrice(),
+//     type: "$",
+//   },
+//   {
+//     productName: "Organic Eggplant",
+//     id: 2,
+//     qty: 0,
+//     src: "./organic-project/assets/product3.jpeg",
+//     price: expPrice(),
+//     type: "$$",
+//   },
+//   {
+//     productName: "Cucumber",
+//     id: 3,
+//     qty: 0,
+//     src: "./organic-project/assets/product4.jpeg",
+//     price: cheapPrice(),
+//     type: "$",
+//   },
+//   {
+//     productName: "Organic Peas",
+//     id: 4,
+//     qty: 0,
+//     src: "./organic-project/assets/product5.jpeg",
+//     price: expPrice(),
+//     type: "$$",
+//   },
+//   {
+//     productName: "Lettuce",
+//     id: 5,
+//     qty: 0,
+//     src: "./organic-project/assets/product6.jpeg",
+//     price: cheapPrice(),
+//     type: "$",
+//   },
+//   {
+//     productName: "Cabbage",
+//     id: 6,
+//     qty: 0,
+//     src: "./organic-project/assets/product7.jpeg",
+//     price: cheapPrice(),
+//     type: "$",
+//   },
+//   {
+//     productName: "Organic Lettuce",
+//     id: 7,
+//     qty: 0,
+//     src: "./organic-project/assets/product8.jpeg",
+//     price: expPrice(),
+//     type: "$$",
+//   },
+// ];
 
 const cart = ["cart is empty"];
 // adding the inventory to database
@@ -96,14 +97,6 @@ const cart = ["cart is empty"];
 // adding cart to data
 // addToDatabase("cart", cart);
 // console.log(cart);
-
-// Importing data from Firebase
-onValue(dbRef, function (snapshot) {
-  const ourData = snapshot.val();
-  // storing the data in inventory variable
-  const inventory = ourData.inventory;
-  displayItems(inventory);
-});
 
 // Function to Display the items on the page
 
@@ -139,3 +132,15 @@ const displayItems = (stock) => {
     productGallery.appendChild(newListItem);
   });
 };
+
+// created a global variable inventory which will store the incoming product infomration from firebase
+let inventory;
+// Importing data from Firebase
+onValue(dbRef, function (snapshot) {
+  const ourData = snapshot.val();
+  // storing the data in inventory variable
+  inventory = ourData.inventory;
+
+  // Inventory is basically the downloaded inventory data from firebase
+  displayItems(inventory);
+});

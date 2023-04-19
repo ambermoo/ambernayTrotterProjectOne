dc830afdb8a5be82fd0e6f5807862b9ba1fb3fb6
rayanMishra <rayanmishra7@gmail.com>
2023-04-19 17:41:06 -0400

    Database fixed

--- script/database.js
+++ script/database.js
@@ -23,72 +23,72 @@ export const cheapPrice = () =>
   parseFloat(Math.random() * (5 - 1) + 1).toFixed(2);
 const expPrice = () => parseFloat(Math.random() * (15 - 10) + 10).toFixed(2);
 
-// const totalInventory = [
-//   {
-//     productName: "Tomatoes",
-//     id: 0,
-//     qty: 0,
-//     src: "./organic-project/assets/product1.jpeg",
-//     price: cheapPrice(),
-//     type: "$",
-//   },
-//   {
-//     productName: "Lime",
-//     id: 1,
-//     qty: 0,
-//     src: "./organic-project/assets/product2.jpeg",
-//     price: cheapPrice(),
-//     type: "$",
-//   },
-//   {
-//     productName: "Organic Eggplant",
-//     id: 2,
-//     qty: 0,
-//     src: "./organic-project/assets/product3.jpeg",
-//     price: expPrice(),
-//     type: "$$",
-//   },
-//   {
-//     productName: "Cucumber",
-//     id: 3,
-//     qty: 0,
-//     src: "./organic-project/assets/product4.jpeg",
-//     price: cheapPrice(),
-//     type: "$",
-//   },
-//   {
-//     productName: "Organic Peas",
-//     id: 4,
-//     qty: 0,
-//     src: "./organic-project/assets/product5.jpeg",
-//     price: expPrice(),
-//     type: "$$",
-//   },
-//   {
-//     productName: "Lettuce",
-//     id: 5,
-//     qty: 0,
-//     src: "./organic-project/assets/product6.jpeg",
-//     price: cheapPrice(),
-//     type: "$",
-//   },
-//   {
-//     productName: "Cabbage",
-//     id: 6,
-//     qty: 0,
-//     src: "./organic-project/assets/product7.jpeg",
-//     price: cheapPrice(),
-//     type: "$",
-//   },
-//   {
-//     productName: "Organic Lettuce",
-//     id: 7,
-//     qty: 0,
-//     src: "./organic-project/assets/product8.jpeg",
-//     price: expPrice(),
-//     type: "$$",
-//   },
-// ];
+const totalInventory = [
+  {
+    productName: "Tomatoes",
+    id: 0,
+    qty: 0,
+    src: "./organic-project/assets/product1.jpeg",
+    price: cheapPrice(),
+    type: "$",
+  },
+  {
+    productName: "Lime",
+    id: 1,
+    qty: 0,
+    src: "./organic-project/assets/product2.jpeg",
+    price: cheapPrice(),
+    type: "$",
+  },
+  {
+    productName: "Organic Eggplant",
+    id: 2,
+    qty: 0,
+    src: "./organic-project/assets/product3.jpeg",
+    price: expPrice(),
+    type: "$$",
+  },
+  {
+    productName: "Cucumber",
+    id: 3,
+    qty: 0,
+    src: "./organic-project/assets/product4.jpeg",
+    price: cheapPrice(),
+    type: "$",
+  },
+  {
+    productName: "Organic Peas",
+    id: 4,
+    qty: 0,
+    src: "./organic-project/assets/product5.jpeg",
+    price: expPrice(),
+    type: "$$",
+  },
+  {
+    productName: "Lettuce",
+    id: 5,
+    qty: 0,
+    src: "./organic-project/assets/product6.jpeg",
+    price: cheapPrice(),
+    type: "$",
+  },
+  {
+    productName: "Cabbage",
+    id: 6,
+    qty: 0,
+    src: "./organic-project/assets/product7.jpeg",
+    price: cheapPrice(),
+    type: "$",
+  },
+  {
+    productName: "Organic Lettuce",
+    id: 7,
+    qty: 0,
+    src: "./organic-project/assets/product8.jpeg",
+    price: expPrice(),
+    type: "$$",
+  },
+];
 
 const cart = ["cart is empty"];
 // adding the inventory to database
